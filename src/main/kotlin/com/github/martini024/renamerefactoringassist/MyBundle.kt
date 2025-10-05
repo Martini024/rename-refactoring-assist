b3c47e315a3009caeee01ac22af6bbbffbf23811
GitHub Action <action@github.com>
2025-10-05 21:45:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/martini024/renamerefactoringassist/MyBundle.kt
+++ src/main/kotlin/com/github/martini024/renamerefactoringassist/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.martini024.renamerefactoringassist
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/martini024/renamerefactoringassist/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/martini024/renamerefactoringassist/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.martini024.renamerefactoringassist.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
